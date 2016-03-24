adfb62cb659c6eed1b012a84816c66f70d673050
Erin Goncer and Nadiya Yeroshkinah <nadiya@>
2016-03-24 15:28:30 -0700

    fixed the calculator logic

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function(event) {
   $("#triangle").submit(function(event) {
-    event.preventDefault();
+
 
   var m1 = parseInt($("input#m1").val());
   var m2 = parseInt($("input#m2").val());
   var m3 = parseInt($("input#m3").val());
+  debugger;
 
-  
-}
+  if (m1 === m2 && m2 === m3) {
+    $(".equilateral").show();
+  }
+
+  else if (m1 === m2) {
+    $(".isosceles").show();
+  }
+  else if (m3 === m2)  {
+    $(".isosceles").show();
+  }
+
+  else {
+    $(".scalene").show();
+  }
+    event.preventDefault();
+  });
+});
