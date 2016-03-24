348d49fe31007f241bf619ef602d7a7dcaa14bd9
Erin Goncer and Nadiya Yeroshkinah <erin@>
2016-03-24 14:43:46 -0700

    create initial webpage files along with styling

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#triangle").submit(function(event) {
+    event.preventDefault();
+
+  var m1 = parseInt($("input#m1").val());
+  var m2 = parseInt($("input#m2").val());
+  var m3 = parseInt($("input#m3").val());
+
+  
+}
